4f306d1127be9df50b23010aaabbbb0820d55356
Artem Brilev <razdvajonez@mail.ru>
2020-08-09 16:52:28 +0300

    webgl

--- index.js
+++ index.js
@@ -1,86 +1,207 @@
-const canvas2d = document.querySelector('#canvas2d');
-canvas2d.height = 2000;
-canvas2d.width = 2000;
-const ctx = canvas2d.getContext('2d');
-ctx.translate(1000, 1000);
+const fragmentShaderSource = `
+#ifdef GL_ES
+precision mediump float;
+#endif
 
-const draw = () => {
-  ctx.clearRect(0, 0, 2000, 2000);
-  ctx.beginPath();
-  ctx.fillStyle = "black";
-  ctx.arc(0, 0, 900, 0, 2*Math.PI, true);
-  ctx.fill();
-  ctx.closePath();
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
 
-  ctx.beginPath();
-  ctx.fillStyle = "white";
-  ctx.arc(0, 0, 830, 0, 2*Math.PI, true);
-  ctx.fill();
-  ctx.closePath();
-
-  ctx.fillStyle = "black";
-  ctx.save();
-  ctx.beginPath();
-  for(let i = 0; i < 12; ++i) {
-      ctx.fillRect(-20, -830, 40, 60);
-      ctx.rotate(Math.PI / 6);
-  }
-  ctx.closePath();
-  ctx.restore();
-
-
-  ctx.save();
-  ctx.beginPath();
-  for(let i = 0; i < 60; ++i) {
-      ctx.fillRect(-20, -830, 20, 40);
-      ctx.rotate(Math.PI / 30);
-  }
-  ctx.closePath();
-  ctx.restore();
-
-  ctx.beginPath();
-  ctx.font = "150px serif"
-  ctx.textAlign = "center";
-  ctx.textBaseline = "middle";
-  for(let i = 0; i < 12; ++i) {
-    const x = 680 * Math.sin(i * Math.PI / 6);
-    const y = -680 * Math.cos(i *Math.PI / 6);
-
-    ctx.fillText(i ? i : 12, x, y);
-  }
-  ctx.closePath();
-  ctx.restore();
-
-  const date = new Date();
-
-  const hour = date.getHours();
-
-  ctx.save();
-  ctx.beginPath();
-  ctx.rotate(Math.PI * hour / 6 );
-  ctx.fillRect(0, -400, 40, 400);
-  ctx.closePath();
-  ctx.restore();
-
-  const minute = date.getMinutes();
-
-  ctx.save();
-  ctx.beginPath();
-  ctx.rotate(Math.PI * minute / 30);
-  ctx.fillRect(0, -600, 30, 600);
-  ctx.closePath();
-  ctx.restore();
-
-  const seconds = date.getSeconds();
-
-  ctx.save();
-  ctx.beginPath();
-  ctx.rotate(Math.PI * seconds / 30);
-  ctx.fillRect(00, -650, 20, 650);
-  ctx.closePath();
-  ctx.restore();
-
-  requestAnimationFrame(draw)
+float random (in vec2 _st) {
+    return fract(sin(dot(_st.xy,
+                         vec2(12.9898,78.233)))*
+        43758.5453123);
 }
 
-draw();
+// Based on Morgan McGuire @morgan3d
+// https://www.shadertoy.com/view/4dS3Wd
+float noise (in vec2 _st) {
+    vec2 i = floor(_st);
+    vec2 f = fract(_st);
+
+    // Four corners in 2D of a tile
+    float a = random(i);
+    float b = random(i + vec2(1.0, 0.0));
+    float c = random(i + vec2(0.0, 1.0));
+    float d = random(i + vec2(1.0, 1.0));
+
+    vec2 u = f * f * (3.0 - 2.0 * f);
+
+    return mix(a, b, u.x) +
+            (c - a)* u.y * (1.0 - u.x) +
+            (d - b) * u.x * u.y;
+}
+
+#define NUM_OCTAVES 5
+
+float fbm ( in vec2 _st) {
+    float v = 0.0;
+    float a = 0.5;
+    vec2 shift = vec2(100.0);
+    // Rotate to reduce axial bias
+    mat2 rot = mat2(cos(0.5), sin(0.5),
+                    -sin(0.5), cos(0.50));
+    for (int i = 0; i < NUM_OCTAVES; ++i) {
+        v += a * noise(_st);
+        _st = rot * _st * 2.0 + shift;
+        a *= 0.5;
+    }
+    return v;
+}
+
+void main() {
+    vec2 st = gl_FragCoord.xy/u_resolution.xy*3.;
+    // st += st * abs(sin(u_time*0.1)*3.0);
+    vec3 color = vec3(0.0);
+
+    vec2 q = vec2(0.);
+    q.x = fbm( st + 0.00*u_time);
+    q.y = fbm( st + vec2(1.0));
+
+    vec2 r = vec2(0.);
+    r.x = fbm( st + 1.0*q + vec2(1.7,9.2)+ 0.15*u_time );
+    r.y = fbm( st + 1.0*q + vec2(8.3,2.8)+ 0.126*u_time);
+
+    float f = fbm(st+r);
+
+    color = mix(vec3(0.101961,0.619608,0.666667),
+                vec3(0.666667,0.666667,0.498039),
+                clamp((f*f)*4.0,0.0,1.0));
+
+    color = mix(color,
+                vec3(0,0,0.164706),
+                clamp(length(q),0.0,1.0));
+
+    color = mix(color,
+                vec3(0.666667,1,1),
+                clamp(length(r.x),0.0,1.0));
+
+    gl_FragColor = vec4((f*f*f+.6*f*f+.5*f)*color,1.);
+}
+`;
+
+const vertexShaderSource = `
+    attribute vec4 a_position;
+
+    // all shaders have a main function
+    void main() {
+
+        // gl_Position is a special variable a vertex shader
+        // is responsible for setting
+        gl_Position = a_position;
+    }
+`;
+
+function createShader(gl, type, source) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+      return shader;
+    }
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+  }
+
+  function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+      return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+  }
+
+  function main() {
+    // Get A WebGL context
+    const canvas2d = document.querySelector('#canvas2d');
+    canvas2d.width = 2550;
+    canvas2d.height = 1640;
+
+    const gl = canvas2d.getContext('webgl');
+    if (!gl) {
+      return;
+    }
+
+
+    // create GLSL shaders, upload the GLSL source, compile the shaders
+    var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+    // Link the two shaders into a program
+    var program = createProgram(gl, vertexShader, fragmentShader);
+
+    // look up where the vertex data needs to go.
+    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+
+    // Create a buffer and put three 2d clip space points in it
+    var positionBuffer = gl.createBuffer();
+
+    // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    var positions = [
+      -1.0, -1.0,
+      1.0, 1.0,
+      1.0, -1.0,
+      -1.0, 1.0,
+      1.0, 1.0,
+      -1.0, -1.0,
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    // code above this line is initialization code.
+    // code below this line is rendering code.
+
+    // Tell WebGL how to convert from clip space to pixels
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    // Clear the canvas
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // Tell it to use our program (pair of shaders)
+    gl.useProgram(program);
+
+    // Turn on the attribute
+    gl.enableVertexAttribArray(positionAttributeLocation);
+
+    // Bind the position buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 2;          // 2 components per iteration
+    var type = gl.FLOAT;   // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+        positionAttributeLocation, size, type, normalize, stride, offset);
+
+    // draw
+    const resolution = gl.getUniformLocation(program, "u_resolution");
+    gl.uniform2fv(resolution, new Float32Array([canvas2d.width, canvas2d.height ]));
+    let delta = 0.0;
+    const draw = () => {
+        const time = gl.getUniformLocation(program, "u_time");
+        gl.uniform1f(time, delta);
+        delta += 0.03;
+        var primitiveType = gl.TRIANGLES;
+        var offset = 0;
+        var count = 6;
+        gl.drawArrays(primitiveType, offset, count);
+        requestAnimationFrame(draw)
+    }
+    draw();
+  }
+
+  main();
+
+
